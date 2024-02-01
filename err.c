4609c28b9e60e7fd045467f54560fb037cee1b8f
ibrahimmar <ibrahimammar811@gmail.com>
2024-02-01 17:29:12 +0200

    frist

--- err.c
+++ err.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
 /**
- * HError - Prints appropriate error messages determined by their error code.
- * @ECode: The error codes are the following:
+ * erro - Prints appropriate error messages determined by their error code.
+ * @erro_code: The error codes are the following:
  * (1) => The user did not provide any file or one file to the program.
  * (2) => The file provided cannot be opened or read.
  * (3) => The file provided contains an invalid instruction.
@@ -12,32 +12,32 @@
  * (7) => When the stack is empty for pop.
  * (8) => When the stack is too short for an operation.
  */
-void HError(int ECode, ...)
+void erro(int erro_code, ...)
 {
 	va_list args;
 	char *instruction;
 	int LNumber;
 
-	va_start(args, ECode);
-	if (ECode == 1)
+	va_start(args, erro_code);
+	if (erro_code == 1)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 	}
-	else if (ECode == 2)
+	else if (erro_code == 2)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
 	}
-	else if (ECode == 3)
+	else if (erro_code == 3)
 	{
 		LNumber = va_arg(args, int);
 		instruction = va_arg(args, char *);
 		fprintf(stderr, "L%d: unknown instruction %s\n", LNumber, instruction);
 	}
-	else if (ECode == 4)
+	else if (erro_code == 4)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 	}
-	else if (ECode == 5)
+	else if (erro_code == 5)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
 	}
@@ -47,64 +47,38 @@ void HError(int ECode, ...)
 }
 
 /**
- * HMErrors - Handles errors.
- * @ECode: The error codes are the following:
+ * many_err - Handles errors.
+ * @erro_code: The error codes are the following:
  * (6) => When the stack is empty for pint.
  * (7) => When the stack is empty for pop.
  * (8) => When the stack is too short for an operation.
  * (9) => Division by zero.
  */
 
-void HMErrors(int ECode, ...)
+void many_err(int err_code, ...)
 {
 	va_list args;
 	char *instruction;
 	int LNumber;
 
-	va_start(args, ECode);
-	if (ECode == 6)
+	va_start(args, err_code);
+	if (err_code == 6)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(args, int));
 	}
-	else if (ECode == 7)
+	else if (err_code == 7)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(args, int));
 	}
-	else if (ECode == 8)
+	else if (err_code == 8)
 	{
 		LNumber = va_arg(args, unsigned int);
 		instruction = va_arg(args, char *);
 		fprintf(stderr, "L%d: can't %s, stack too short\n", LNumber, instruction);
 	}
-	else if (ECode == 9)
+	else if (err_code == 9)
 	{
 		fprintf(stderr, "L%d: division by zero\n", va_arg(args, unsigned int));
-	}
-	va_end(args);
-	free_nodes();
-	exit(EXIT_FAILURE);
-}
-
-/**
- * HSErrors - Handles errors.
- * @ECode: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
- */
-void HSErrors(int ECode, ...)
-{
-	va_list args;
-	int LNumber;
-
-	va_start(args, ECode);
-	LNumber = va_arg(args, int);
-	if (ECode == 10)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", LNumber);
-	}
-	else if (ECode == 11)
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", LNumber);
 	}
 	va_end(args);
 	free_nodes();
