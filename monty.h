29c80e3081edd565af67251ec6971aff7a551413
Asmaa231 <asmaasalem231@gmail.com>
2024-02-01 09:44:47 -0800

    a

--- monty.h
+++ monty.h
@@ -2,6 +2,8 @@
 #define MONTY_H
 
 #define _GUN_SOURCE
+#define _XOPEN_SOURCE 700
+#define _POSIX_C_SOURCE 200809L
 
 #include <string.h>
 #include <stdio.h>
@@ -49,12 +51,12 @@ void CallOpcode(op_fun, char *opcode, char *value, int LNumber, int format);
 /*stack process*/
 stack_t *make_node(int x);
 void fr_node(void);
-void print_st(stack_t **, unsigned int);
-void add_st(stack_t **, unsigned int);
+void PstackContents(stack_t **, unsigned int);
+void AddNodeToStack(stack_t **, unsigned int);
 void add_qu(stack_t **, unsigned int);
 
-void top_print(stack_t **, unsigned int);
-void (stack_t **, unsigned int);
+void PTopOfStack(stack_t **, unsigned int);
+void PopFromStack(stack_t **, unsigned int);
 void swap(stack_t **, unsigned int);
 void nop(stack_t **, unsigned int);
 
